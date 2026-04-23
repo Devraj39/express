9c59b89baed86c496028b0beacc1cc2449bbd2c4
Devraj Singh <143243098+Devraj39@users.noreply.github.com>
2026-04-23 10:37:58 +0530

    Add files via upload

--- employeeController.js
+++ employeeController.js
@@ -2,3 +2,14 @@ exports.addemployee = (req,res) => {
     res.end("Employee Added")
 }
 
+exports.aggsample = async (req,res) => {
+    const aggregateData = EmployeeModel.aggregate([
+        {$match: {department: "sales"}},  // filter
+        {$group: {_id: "$department",count: {$sum: 1}}}, // form a grp of all the employee from sales department 
+        {$sort: {_id: 1}},  //sort in alphabetic order
+        {$limit: 2} // any 2 dept at a time 
+    ]);
+    res.end(aggregateData);
+}
+
+// display top salary employee for each dept
